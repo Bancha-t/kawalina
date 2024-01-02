3bc5e7caec906fc7ee0d66eafe826fd0f69d5810
Bancha-t <gmtv7887@gmail.com>
2024-01-02 16:34:40 +0700

    GGkub

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -17,3 +17,27 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+void stat(const double A[] , int N , double B[] ){
+    double x = 0, xb , max = A[0] , min = A[0] , sum = 0 , GM = 1 , HM = 0 ;
+    for(int i = 0 ; i < N ; i++){
+        sum += A[i] ;
+        xb = sum / N ;
+        GM *= A[i] ;
+        HM += 1/A[i] ;
+
+    }
+
+
+    for(int i = 0 ; i < N ; i++){  
+        x += pow(A[i]-xb ,2) ;
+        if( A[i] > max ) max = A[i] ;
+        if( A[i] < min ) min  = A[i] ;
+    }
+    B[0] = sum / N ;
+    B[1] = sqrt(x/N-1) ;
+    B[2] = pow(GM , 1.0/N) ;
+    B[3] = N / HM ;
+    B[4] = max ;
+    B[5] = min ;
+
+}
